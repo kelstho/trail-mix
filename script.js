ddfb269a233c05b56157d605d66a17dd1e693845
INWYTIA <inwytia@gmail.com>
2020-01-22 19:18:35 -0600

    html populator

--- script.js
+++ script.js
@@ -12,12 +12,23 @@ $('#location-search').on('click', function() {
         console.log(lat);
         var lon = geoLocate.results[0].locations[0].latLng.lng;
         console.log(lon);
-        var trailFindQuery = 'https://www.mtbproject.com/data/get-trails?lat=' + lat + '&lon=' + lon + '&key=200670702-d6c5c4298cb260abb7cfae993c480ef4';
+        var trailFindQuery = 'https://www.mtbproject.com/data/get-trails?lat=' + lat + '&lon=' + lon + '&maxResults=5&key=200670702-d6c5c4298cb260abb7cfae993c480ef4';
         $.ajax({
             url : trailFindQuery,
             method : 'GET',
         }).done(function (response) {
+            response.trails.forEach( function (newTrail) {
+                var newRow = $('<tr>');
+                var newName = $('<td>');
+                var newLoc = $('<td>');
+                var newLeng = $('<td>');
+                newName.text(newTrail.name);
+                newLoc.text(newTrail.location);
+                newLeng.text(newTrail.length + 'miles');
+            });
             console.log(response)
         });
     });
 })
+
+
