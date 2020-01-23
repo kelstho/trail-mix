2be299f4c746fb669977279e06f335287d25bbef
INWYTIA <inwytia@gmail.com>
2020-01-22 20:35:29 -0600

    adding attributes for lat and lon

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ $('#location-search').on('click', function() {
         console.log(lat);
         var lon = geoLocate.results[0].locations[0].latLng.lng;
         console.log(lon);
-        var trailFindQuery = 'https://www.mtbproject.com/data/get-trails?lat=' + lat + '&lon=' + lon + '&maxResults=5&key=200670702-d6c5c4298cb260abb7cfae993c480ef4';
+        var trailFindQuery = 'https://www.mtbproject.com/data/get-trails?lat=' + lat + '&lon=' + lon + '&maxResults=5&sort=distance&key=200670702-d6c5c4298cb260abb7cfae993c480ef4';
         $.ajax({
             url : trailFindQuery,
             method : 'GET',
@@ -24,6 +24,7 @@ $('#location-search').on('click', function() {
                 var newName = $('<td>');
                 var newLoc = $('<td>');
                 var newLeng = $('<td>');
+                newRow.attr({class: 'myTrail' ,dataloc: newTrail.latitude + ',' + newTrail.longitude})
                 newImg.attr('src', newTrail.imgSqSmall);
                 newPic.append(newImg);
                 newName.text(newTrail.name);
