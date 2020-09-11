c8b55fe2885e0a35024bac742beb4184ac938fb2
Kelly Thompson <ksthompson48@gmail.com>
2020-09-11 11:21:09 -0500

    format js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-$('#location-search').on('click', function() {
+$('#location-search').on('click', function () {
     $('.all-results').empty();
     $('.results-header').text('Trails');
     var location = $('#location-start').val();
     var mapquestQuery = 'https://open.mapquestapi.com/geocoding/v1/address?key=zFTKFOl5heUyBHHsvaEVGGlUnB0XQipR&location=' + location;
     $.ajax({
-        url : mapquestQuery,
-        method : 'GET',
+        url: mapquestQuery,
+        method: 'GET',
     }).done(function (geoLocate) {
         console.log(geoLocate)
         var lat = geoLocate.results[0].locations[0].latLng.lat;
@@ -14,11 +14,11 @@ $('#location-search').on('click', function() {
         console.log(lon);
         var trailFindQuery = 'https://www.mtbproject.com/data/get-trails?lat=' + lat + '&lon=' + lon + '&maxResults=5&sort=distance&key=200670702-d6c5c4298cb260abb7cfae993c480ef4';
         $.ajax({
-            url : trailFindQuery,
-            method : 'GET',
+            url: trailFindQuery,
+            method: 'GET',
         }).done(function (response) {
             var iter = 0;
-            response.trails.forEach( function (newTrail) {
+            response.trails.forEach(function (newTrail) {
                 var newtarg = 'targ' + iter;
                 var newRow = $('<div>');
                 var newPic = $('<div>');
@@ -27,7 +27,7 @@ $('#location-search').on('click', function() {
                 var newLoc = $('<div>');
                 var newLeng = $('<div>');
                 var newFood = $('<div>');
-                newRow.attr({class: 'myTrail row', dataloc: newTrail.latitude + ',' + newTrail.longitude, datatarget: newtarg});
+                newRow.attr({ class: 'myTrail row', dataloc: newTrail.latitude + ',' + newTrail.longitude, datatarget: newtarg });
                 newImg.attr('src', newTrail.imgSqSmall);
                 newPic.attr('class', 'col s12 m3 trail-image');
                 newName.attr('class', 'col s12 m3 trail-results');
@@ -52,18 +52,18 @@ $('#location-search').on('click', function() {
     });
 });
 
-$(document).on("click", ".myTrail", function(){
+$(document).on("click", ".myTrail", function () {
     var latLng = $(this).attr("dataloc");
     var myTarg = $(this).attr('datatarget');
     searchRes(latLng, myTarg);
 });
 
-function searchRes(myLoc, target){
+function searchRes(myLoc, target) {
     var myUrl = "https://www.mapquestapi.com/search/v2/radius?origin=" + myLoc + "&radius=10&maxMatches=4&ambiguities=ignore&hostedData=mqap.ntpois|group_sic_code=?|581208&outFormat=json&key=o0XevQpqpVayxArGM1iZ5UuLTnHchJUr";
-$.ajax({
-        url : myUrl,
-        method : 'GET'
-    }).done(function (data){
+    $.ajax({
+        url: myUrl,
+        method: 'GET'
+    }).done(function (data) {
         var targetDiv = $('.' + target);
         if (targetDiv.html() === 'Click above to show or hide restaurants for that trail.') {
             targetDiv.empty();
@@ -78,8 +78,8 @@ $.ajax({
                 locDiv.attr('class', 'rest-address');
                 distDiv.attr('class', 'rest-distance');
                 nameDiv.text(restraunt.name);
-                locDiv.text(restraunt.fields.address+ " " + restraunt.fields.city+ " " +restraunt.fields.state);
-                distDiv.text('Distance: '+restraunt.distance+" "+restraunt.distanceUnit);
+                locDiv.text(restraunt.fields.address + " " + restraunt.fields.city + " " + restraunt.fields.state);
+                distDiv.text('Distance: ' + restraunt.distance + " " + restraunt.distanceUnit);
                 myResult.append(nameDiv);
                 myResult.append(locDiv);
                 myResult.append(distDiv);
@@ -90,4 +90,4 @@ $.ajax({
             targetDiv.text('Click above to show or hide restaurants for that trail.');
         };
     });
-} ;
+};
